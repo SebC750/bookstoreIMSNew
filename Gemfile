3d6834cd1eb861fbcb43a8fbcbe36bb225eb8798
SebC750 <SebC89999@gmail.com>
2025-03-31 19:12:13 -0400

    RSpec testing suites - Created testing suites for book list, book and user, validating proper behavior for the app. - Minor styling corrections.

--- Gemfile
+++ Gemfile
@@ -45,7 +45,8 @@ gem "devise"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
-
+  gem "rspec-rails", "~> 6.1.3"
+  gem "factory_bot_rails"
   # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
   gem "brakeman", require: false
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, :password, :password_confirmation, presence: { message: "Please enter a value." }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Email must be written in the correct format ([name]@[mailservice].com)" }
+  validates :password, presence: true, length: { minimum: 6, message: "Password must have a minimum of 6 characters." }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :book_lists
+  has_many :book_lists, dependent: :destroy
 end
